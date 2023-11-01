bc87d6d82d5f6df347c8e26b7488be5f255020b8
richardlzluo <richard.lz.luo@gmail.com>
2023-11-01 22:08:20 +0800

    commit

--- paths.py
+++ paths.py
@@ -1,19 +1,19 @@
 class PATHS:
-    BASE_PATH = "/kaggle/"
+    BASE_PATH = "/kaggle/input/rsna-2023-abdominal-trauma-detection"
 
-    OUTPUT_BASE = f'{BASE_PATH}/KAGGLE_SUBMISSION/'
+    OUTPUT_BASE = f'/kaggle/working/output/'
 
     CONTRAIL_MODEL_BASE = f'{BASE_PATH}/src_contrails/'
     COAT_PRETRAINED = f'{BASE_PATH}/coat_pretrained/'
 
-    SEGMENTOR_SAVE_FOLDER = f'{OUTPUT_BASE}/segmentation_data_v1/'
+    SEGMENTOR_SAVE_FOLDER = '/kaggle/input/preprocess-data-seg/Data/KAGGLE_SUBMISSION/segmentation_data_v1'
 
     TOTAL_SEGMENTOR_FOLDER = f'{BASE_PATH}/TotalSegmentor/Totalsegmentator_dataset/'
-    TOTAL_SEGMENTOR_SAVE_FOLDER = f'{OUTPUT_BASE}/total_data_v1/'
+    TOTAL_SEGMENTOR_SAVE_FOLDER = '/kaggle/input/preprocess-data-seg/Data/KAGGLE_SUBMISSION/total_data_v1'
 
-    SEGMENTATION_MODEL_SAVE = f'{OUTPUT_BASE}/SEG_MODELS/'
+    SEGMENTATION_MODEL_SAVE = '/kaggle/input/preprocess-data-seg/Data/KAGGLE_SUBMISSION/SEG_MODELS'
 
-    INFO_DATA_SAVE = f'{BASE_PATH}/KAGGLE_SUBMISSION/info_data1_processed_v1.csv'
+    INFO_DATA_SAVE = '/kaggle/input/preprocess-data-seg/Data/KAGGLE_SUBMISSION/info_data1_processed_v1.csv'
 
     THEO_DATA_PATH = f'{BASE_PATH}/theo_images/'
     THEO_SAVE_PATH = f'{BASE_PATH}/KAGGLE_SUBMISSION/theo_data/'
